b978711511e0d614a4f1e1b804374f59693a3dbe
James Hillyerd <james@hillyerd.com>
2024-02-21 14:24:19 -0800

    init: empty mdbook
    Signed-off-by: James Hillyerd <james@hillyerd.com>

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+
+          scripts.serve = pkgs.writeShellScriptBin "serve" ''
+            cd "$(git rev-parse --show-toplevel)"
+            ${pkgs.mdbook}/bin/mdbook serve
+          '';
+        in
+        {
+          devShell = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              mdbook
+
+              scripts.serve
+            ];
+          };
+
+          shellHook = ''
+            echo "Commands:"
+            echo "  serve - runs mdbook to watch & serve"
+          '';
+        }
+      );
+}
